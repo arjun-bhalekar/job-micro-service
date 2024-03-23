c1670a9a836fb224fdd397d301be0f4190d00d4d
arjun-bhalekar <arjunbhalekar95@gmail.com>
2024-03-23 11:20:46 +0530

    initial code

---
+++ src/main/java/com/sb/app/job/ms/JobMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sb.app.job.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMicroServiceApplication.class, args);
+	}
+
+}
