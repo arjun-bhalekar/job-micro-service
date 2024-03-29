9974ef310a0c685648d8788ca46ebae41e6d9314
arjun-bhalekar <arjunbhalekar95@gmail.com>
2024-03-29 12:42:51 +0530

    Spring Retry integration

--- src/main/java/com/sb/app/job/ms/JobMicroServiceApplication.java
+++ src/main/java/com/sb/app/job/ms/JobMicroServiceApplication.java
@@ -3,10 +3,12 @@ package com.sb.app.job.ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableRetry
 public class JobMicroServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sb/app/job/ms/feignclients/CompanyClient.java
+++ src/main/java/com/sb/app/job/ms/feignclients/CompanyClient.java
@@ -2,6 +2,8 @@ package com.sb.app.job.ms.feignclients;
 
 import com.sb.app.job.ms.external.Company;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -9,5 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface CompanyClient {
 
     @GetMapping("/companies/{id}")
+    @Retryable(maxAttempts = 5)
     Company getCompany(@PathVariable Long id);
 }
